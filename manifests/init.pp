ed88231a5b2455445e4b0020a84124c6d5712081
Ewoud Kohl van Wijngaarden <github@kohlvanwijngaarden.nl>
2017-06-29 17:08:14 +0200

    Modernize the module
    * Drop the unused dependency on puppetlabs-stdlib
    * Use hiera to load variables rather than params.pp
    * Move package installation from git::install to git
    * Add Puppet 4 type checks
    * Make the binary path a parameter to init so it can
      be overridden globally

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,28 @@
 # Sets up requirements for git. See git::repo for more information on how to
 # use this module.
 #
+# == Parameters:
+#
+# $bin:: The path to the git binary
+#
+# $package::  Override the name of the git package(s) to include.
+#
+# $package_ensure:: Override the git package ensure
+#
+# == Usage:
+#
+# Example: Override the git version
+#
+#   class { 'git':
+#     package_ensure => '2.1.0',
+#   }
+#
 class git (
-  $package        = $::git::params::package,
-  $package_ensure = $::git::params::package_ensure,
-) inherits ::git::params {
-  include ::git::install
+  $bin = undef,
+  $package = undef,
+  $package_ensure = undef,
+) {
+  package { $package:
+    ensure => $package_ensure,
+  }
 }
