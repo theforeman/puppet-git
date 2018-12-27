30e7ca051f425afe4a2dc9c0434afaaf809ede27
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-12-27 22:52:27 +0100

    Convert documentation to puppet-strings

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,14 @@
-# =Class: git
+# Sets up requirements for git
 #
-# Sets up requirements for git. See git::repo for more information on how to
-# use this module.
+# @see git::repo on how to use this module.
 #
-# == Parameters:
+# @param bin The path to the git binary
 #
-# $bin:: The path to the git binary
+# @param package Override the name of the git package(s) to include.
 #
-# $package::  Override the name of the git package(s) to include.
+# @param package_ensure Override the git package ensure
 #
-# $package_ensure:: Override the git package ensure
-#
-# == Usage:
-#
-# Example: Override the git version
-#
+# @example Override the git version
 #   class { 'git':
 #     package_ensure => '2.1.0',
 #   }
